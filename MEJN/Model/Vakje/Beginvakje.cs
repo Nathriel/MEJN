1708ee7af56f9959e48ef2dfe7b99d8b8b614c66
Hidde Jansen <webmaster@electricnation.nl>
2011-03-24 23:05:26 +0100

    Nieuwe GUI

---
+++ MEJN/Model/Vakje/Beginvakje.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MEJN.Model
+{
+	class Beginvakje : Vakje
+	{
+		protected Kleur kleur;
+
+		public Kleur Kleur
+		{
+			get { return kleur; }
+			set { kleur = value; }
+		}
+
+		public Beginvakje(String soort)
+			: base(soort)
+		{
+
+		}
+	}
+}
---
+++ MEJN/Model/Vakje/Finishvakje.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MEJN.Model
+{
+	class Finishvakje : Vakje
+	{
+		protected Kleur kleur;
+
+		public Kleur Kleur
+		{
+			get { return kleur; }
+			set { kleur = value; }
+		}
+
+		public Finishvakje(String soort)
+			: base(soort)
+		{
+
+		}
+	}
+}
