f1615a257ab42ebc54277ccb1912c69f5aaa8f18
Hidde Jansen <webmaster@electricnation.nl>
2011-03-30 18:06:25 +0200

    Icon that does not work.

--- MEJN/obj/x86/Debug/MEJN_Content.g.i.cs
+++ MEJN/obj/x86/Debug/MEJN_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("turnindicatornotactive.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("menslogo.ico")]
 
 
